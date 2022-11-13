c497f12aae3dd547c85d1f90794118c8d1b2648c
Kurt L. Manion <klmanion@gmail.com>
2022-11-12 21:01:04 -0500

    removed old code

--- src/typefight.cpp
+++ src/typefight.cpp
@@ -42,7 +42,6 @@ typefight()
 			break;;
 
 		default:
-//			keymap.invoke(Keyseq(string(1, (char)(ch & 0x7F))));	// TODO accept ncurses output for keyseq ctor
 			keymap.invoke(Keyseq(ch));
 			break;;
 		}
